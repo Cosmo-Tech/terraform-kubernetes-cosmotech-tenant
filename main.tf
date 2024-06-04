44d63105710b48ad4111cd7b19433e0d3e5f20ff
enocquet <emmanuelnocquet8@gmail.com>
2024-06-04 10:57:27 +0200

    flaten module

--- main.tf
+++ main.tf
@@ -1,15 +1,40 @@
 locals {
+  tls_secret_name = var.tls_certificate_type != "none" ? var.tls_secret_name : ""
+}
+
+resource "kubernetes_namespace" "main_namespace" {
+  metadata {
+    name = var.kubernetes_tenant_namespace
+  }
+}
+
+resource "random_password" "redis_admin_password" {
+  length  = 30
+  special = false
+}
+
+resource "random_password" "argo_minio_secret_key" {
+  length  = 30
+  special = false
+}
+
+resource "random_password" "argo_minio_access_key" {
+  length  = 30
+  special = false
+}
+
+locals {
   # host                   = local.kube_config.0.host
   # client_certificate     = base64decode(local.kube_config.0.client_certificate)
   # client_key             = base64decode(local.kube_config.0.client_key)
   # cluster_ca_certificate = base64decode(local.kube_config.0.cluster_ca_certificate)
-  tags = {
-    vendor      = "cosmotech"
-    stage       = var.project_stage
-    customer    = var.customer_name
-    project     = var.project_name
-    cost_center = var.cost_center
-  }
+  # tags = {
+  #   vendor      = "cosmotech"
+  #   stage       = var.project_stage
+  #   customer    = var.customer_name
+  #   project     = var.project_name
+  #   cost_center = var.cost_center
+  # }
 }
 
 provider "kubernetes" {
@@ -40,9 +65,4 @@ provider "kubectl" {
 
   config_path      = "config"
   load_config_file = true
-}
-
-locals {
-  # kube_config     = data.azurerm_kubernetes_cluster.current.kube_config
-  tls_secret_name = var.tls_certificate_type == "let_s_encrypt" ? "letsencrypt-prod" : "custom-tls-secret"
 }
