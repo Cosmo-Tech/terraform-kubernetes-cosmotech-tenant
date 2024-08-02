d82d9b3bc1f02f6a1b5d9b62d56a2594f8c03b36
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-08-02 11:09:10 +0200

    Add refactor variables

---
+++ variables.client.tf
@@ -0,0 +1,9 @@
+variable "client_id" {
+  description = "The client id of the app registration used to build this"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret of the app registration used to build this"
+  type        = string
+}
