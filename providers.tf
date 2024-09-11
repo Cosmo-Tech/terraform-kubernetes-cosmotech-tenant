14aa0b23ce4452d5dc91ff2f97bb0fa566712b19
nibaldo <nibaldo.donoso@cosmotech.com>
2024-09-11 14:53:45 +0200

    add resource_provider_registrations none [skip ci]

--- providers.tf
+++ providers.tf
@@ -35,6 +35,7 @@ provider "keycloak" {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  resource_provider_registrations = "none"
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
