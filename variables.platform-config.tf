7c642e73ab36dca70cc661d29acbc11a52247d12
nibaldo <nibaldo.donoso@cosmotech.com>
2024-09-13 22:55:43 +0200

    fix helm provider and variables [skip ci]

--- variables.platform-config.tf
+++ variables.platform-config.tf
@@ -4,10 +4,12 @@ variable "create_platform_config" {
 
 variable "identity_authorization_url" {
   type = string
+  default = ""
 }
 
 variable "identity_token_url" {
   type = string
+  default = ""
 }
 
 variable "acr_login_server_url" {
