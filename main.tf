ebc5486b92aae11091ff1e644d5db58e911d1078
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-10-23 17:10:40 +0200

    Move redis version validation to vars [skip-ci]

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 locals {
-  kube_config             = data.azurerm_kubernetes_cluster.current.kube_config
-  tls_secret_name         = var.tls_certificate_type == "let_s_encrypt" ? "letsencrypt-prod" : "custom-tls-secret"
-  use_minio_storage       = !startswith(var.cosmotech_api_version, "1.")
-  use_new_redis           = (tonumber(split(".", var.cosmotech_api_version)[0]) == 3 && tonumber(split(".", var.cosmotech_api_version)[1]) >= 2) || (tonumber(split(".", var.cosmotech_api_version)[0]) > 3)
-  version_redis_cosmotech = local.use_new_redis ? "1.0.12" : "1.0.8"
-  host                    = local.kube_config.0.host
-  client_certificate      = base64decode(local.kube_config.0.client_certificate)
-  client_key              = base64decode(local.kube_config.0.client_key)
-  cluster_ca_certificate  = base64decode(local.kube_config.0.cluster_ca_certificate)
+  kube_config            = data.azurerm_kubernetes_cluster.current.kube_config
+  tls_secret_name        = var.tls_certificate_type == "let_s_encrypt" ? "letsencrypt-prod" : "custom-tls-secret"
+  use_minio_storage      = !startswith(var.cosmotech_api_version, "1.")
+  host                   = local.kube_config.0.host
+  client_certificate     = base64decode(local.kube_config.0.client_certificate)
+  client_key             = base64decode(local.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(local.kube_config.0.cluster_ca_certificate)
 }
 
 provider "kubernetes" {
