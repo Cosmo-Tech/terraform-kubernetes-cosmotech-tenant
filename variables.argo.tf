e6542e681bb7d5622b9f1df377c1ee1d430dead3
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-10-28 10:51:25 +0100

    Move version validation to variables.tf [skip ci]

--- variables.argo.tf
+++ variables.argo.tf
@@ -8,6 +8,19 @@ variable "argo_helm_chart" {
 
 variable "argo_version" {
   type = string
+  validation {
+    condition = (
+      (
+        (tonumber(split(".", var.argo_version)[0]) == 0 &&
+          tonumber(split(".", var.argo_version)[1]) == 16 &&
+          tonumber(split(".", var.argo_version)[2]) < 6
+        ) ||
+        (tonumber(split(".", var.argo_version)[0]) == 0 &&
+        tonumber(split(".", var.argo_version)[1]) < 16)
+      )
+    )
+    error_message = "The Argo Chart version must be less than 0.16.6."
+  }
 }
 
 variable "argo_service_account" {
