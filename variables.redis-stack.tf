0a60b23c110c6b3e48a2ece1fda100595965ee6e
nibaldo <nibaldo.donoso@cosmotech.com>
2024-11-12 17:16:36 +0100

    fix validation

--- variables.redis-stack.tf
+++ variables.redis-stack.tf
@@ -34,17 +34,4 @@ variable "redis_admin_password" {
 variable "version_redis_cosmotech" {
   type        = string
   description = "Redis version"
-
-  validation {
-    condition = (
-      (
-        (tonumber(split(".", var.cosmotech_api_version)[0]) == 3 &&
-        tonumber(split(".", var.cosmotech_api_version)[1]) >= 2) ||
-        (tonumber(split(".", var.cosmotech_api_version)[0]) > 3)
-      ) ?
-      var.version_redis_cosmotech == "1.0.12" :
-      var.version_redis_cosmotech == "1.0.8"
-    )
-    error_message = "Redis version must be 1.0.12 for API >= 3.2.0, or 1.0.8 for previous versions"
-  }
 }
