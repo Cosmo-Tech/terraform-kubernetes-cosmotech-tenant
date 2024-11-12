054386e9d46d92214ece18e8e403a023d0d6ff39
nibaldo <nibaldo.donoso@cosmotech.com>
2024-11-12 17:08:22 +0100

    add new values by default in cosmotech api

--- create-cosmotech-api/main.tf
+++ create-cosmotech-api/main.tf
@@ -1,12 +1,12 @@
 locals {
-  local_acr_login_password   = var.acr_login_password == "" ? data.kubernetes_secret.acr_login_password.data.password : var.acr_login_password
-  local_acr_login_registry   = var.acr_login_server == "" ? data.kubernetes_secret.acr_login_password.data.registry : var.acr_login_server
-  local_acr_login_username   = var.acr_login_username == "" ? data.kubernetes_secret.acr_login_password.data.username : var.acr_login_username
-  local_storage_account_key  = var.storage_account_key == "" ? data.kubernetes_secret.storage_account_password.data.password : var.storage_account_key
-  local_storage_account_name = var.storage_account_name == "" ? data.kubernetes_secret.storage_account_password.data.name : var.storage_account_name
-  local_instance_name        = "${var.helm_release_name}-${var.kubernetes_tenant_namespace}"
-  local_tls_secret_name      = "${var.tls_secret_name}-${var.kubernetes_tenant_namespace}"
-  local_identifier_uri       = var.identifier_uri != "" ? var.identifier_uri : "https://${var.api_dns_name}"
+  local_acr_login_password       = var.acr_login_password == "" ? data.kubernetes_secret.acr_login_password.data.password : var.acr_login_password
+  local_acr_login_registry       = var.acr_login_server == "" ? data.kubernetes_secret.acr_login_password.data.registry : var.acr_login_server
+  local_acr_login_username       = var.acr_login_username == "" ? data.kubernetes_secret.acr_login_password.data.username : var.acr_login_username
+  local_storage_account_key      = var.storage_account_key == "" ? data.kubernetes_secret.storage_account_password.data.password : var.storage_account_key
+  local_storage_account_name     = var.storage_account_name == "" ? data.kubernetes_secret.storage_account_password.data.name : var.storage_account_name
+  local_instance_name            = "${var.helm_release_name}-${var.kubernetes_tenant_namespace}"
+  local_tls_secret_name          = "${var.tls_secret_name}-${var.kubernetes_tenant_namespace}"
+  local_identifier_uri           = var.identifier_uri != "" ? var.identifier_uri : "https://${var.api_dns_name}"
   local_network_sp_client_secret = var.network_sp_client_secret == "" ? data.kubernetes_secret.network_client_password.data.password : var.network_sp_client_secret
   local_tenant_sp_client_secret  = var.tenant_sp_client_secret == "" ? data.kubernetes_secret.platform_client_password.data.password : var.tenant_sp_client_secret
   local_adx_ingestion_uri        = var.adx_ingestion_uri == "" ? data.kubernetes_secret.adx_ingestion_uri_secret.data.uri : var.adx_ingestion_uri
@@ -55,10 +55,13 @@ locals {
     "RABBITMQ_SENDER_USERNAME"      = var.rabbitmq_sender_username
     "RABBITMQ_SENDER_PASSWORD"      = var.rabbitmq_sender_password
     "ALLOWED_API_KEY_CONSUMERS"     = jsonencode(var.list_apikey_allowed)
+    "AUTHORIZED_MIME_TYPES"         = jsonencode(var.list_authorized_mime_types)
     "PERSISTENCE_SIZE"              = var.persistence_size
     "PERSISTENCE_STORAGE_CLASS"     = var.persistence_storage_class
     "KEYCLOAK_CLIENT_ID"            = var.keycloak_client_id
     "KEYCLOAK_CLIENT_SECRET"        = var.keycloak_client_secret
+    "MAX_FILE_SIZE"                 = var.max_file_size
+    "MAX_REQUEST_SIZE"              = var.max_request_size
   }
 }
 
