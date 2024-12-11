6475c53c90ea4fa2672259305f3a6aef72954e2e
Barny <barnabas.lukacs.ext@cosmotech.com>
2024-12-11 16:46:37 +0200

    modeling api deployment

---
+++ create-modeling-api-deployment/main.tf
@@ -0,0 +1,31 @@
+locals {
+  local_instance_name            = "${var.helm_release_name}-${var.kubernetes_tenant_namespace}"
+  
+
+  values_modeling_api = {
+    "MONITORING_ENABLED"            = var.monitoring_enabled
+    "MONITORING_NAMESPACE"          = var.monitoring_namespace
+    "NAMESPACE"                     = var.kubernetes_tenant_namespace
+    "COSMOTECH_API_DNS_NAME"        = var.api_dns_name
+    "PERSISTENCE_STORAGE_CLASS"     = var.persistence_storage_class
+    "WEBHOOK_SECRET"                = var.wehbook_secret
+    "DOCKER_SECRET"                 = var.docker_secret
+    "ARGO_SERVICE_ACCOUNT"          = var.argo_service_account
+    "ARGO_RELEASE_NAME"             = var.argo_release_name
+  }
+}
+
+resource "helm_release" "modeling-api-deployment" {
+  name       = local.local_instance_name
+  repository = var.helm_repository
+  chart      = var.helm_chart
+  version    = var.chart_package_version
+  namespace  = var.kubernetes_tenant_namespace
+
+  reset_values = true
+  timeout      = 600
+
+  values = [
+    templatefile("${path.module}/values-modeling-api.yaml", local.values_modeling_api)
+  ]
+}
---
+++ create-modeling-api-deployment/variables.tf
@@ -0,0 +1,56 @@
+
+
+variable "monitoring_enabled" {
+  type = bool
+}
+
+variable "monitoring_namespace" {
+  type = string
+}
+
+variable "api_dns_name" {
+  type = string
+}
+
+
+variable "helm_chart" {
+  type    = string
+}
+
+variable "helm_repository" {
+  type    = string
+}
+
+
+variable "helm_release_name" {
+  type    = string
+}
+
+variable "chart_package_version" {
+  type = string
+}
+
+
+variable "persistence_storage_class" {
+  type = string
+}
+
+variable "kubernetes_tenant_namespace" {
+  type = string
+}
+
+variable "wehbook_secret" {
+  type = string
+}
+
+variable "docker_secret" {
+  type = string
+}
+
+variable "argo_service_account" {
+  type    = string
+}
+
+variable "argo_release_name" {
+  type = string
+}
