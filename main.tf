d24edbd85ba38baaed122851bba409c9c6349064
nibaldo <nibaldo.donoso@cosmotech.com>
2024-12-13 21:09:58 +0100

    add rbac

--- main.tf
+++ main.tf
@@ -1,36 +1,11 @@
 locals {
-  kube_config            = data.azurerm_kubernetes_cluster.current.kube_config
+  kube_config            = data.azurerm_kubernetes_cluster.current.kube_admin_config
   tls_secret_name        = var.tls_certificate_type == "let_s_encrypt" ? "letsencrypt-prod" : "custom-tls-secret"
   use_minio_storage      = !startswith(var.cosmotech_api_version, "1.")
   host                   = local.kube_config.0.host
   client_certificate     = base64decode(local.kube_config.0.client_certificate)
   client_key             = base64decode(local.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(local.kube_config.0.cluster_ca_certificate)
-}
-
-provider "kubernetes" {
-  host                   = local.host
-  client_certificate     = local.client_certificate
-  client_key             = local.client_key
-  cluster_ca_certificate = local.cluster_ca_certificate
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = local.host
-    client_certificate     = local.client_certificate
-    client_key             = local.client_key
-    cluster_ca_certificate = local.cluster_ca_certificate
-  }
-}
-
-provider "kubectl" {
-  host                   = local.host
-  client_certificate     = local.client_certificate
-  client_key             = local.client_key
-  cluster_ca_certificate = local.cluster_ca_certificate
-
-  load_config_file = false
 }
 
 data "azurerm_kubernetes_cluster" "current" {
