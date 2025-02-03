8ec23a0c7736f2a38cac09d506750cf46ff59040
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-03 14:39:30 +0100

    add seaweedfs pvc and remove replica veriable in pvc postgres

---
+++ persistence-claim-seaweedfs/main.tf
@@ -0,0 +1,19 @@
+locals {
+  disk_name  = "disk-seaweedfs-tenant-${var.kubernetes_tenant_namespace}"
+}
+
+resource "kubernetes_persistent_volume_claim" "seaweedfs_master" {
+  metadata {
+    name      = "pvc-${local.disk_name}"
+    namespace = var.kubernetes_tenant_namespace
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "${var.pvc_seaweedfs_storage_gbi}Gi"
+      }
+    }
+    volume_name = "pv-${local.disk_name}"
+  }
+}
--- variables.pvc-postgres.tf
+++ variables.pvc-postgres.tf
@@ -1,6 +1,3 @@
 variable "pvc_postgres_storage_gbi" {
   type = number
 }
-variable "pvc_postgres_replicas" {
-  type = number
-}
