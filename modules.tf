1b211132e97cadf1bb3e3b5b55f9bbcac1ca8c2f
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-24 14:15:38 +0100

    remove local use minio var

--- modules.tf
+++ modules.tf
@@ -8,12 +8,12 @@ module "create-argo" {
   postgres_argo_user          = module.create-postgresql-db.0.out_argo_postgresql_user
   postgres_release_name       = module.create-postgresql-db.0.out_postgres_release_name
   s3_bucket_name              = var.s3_bucket_name
-  s3_endpoint                 = local.use_minio_storage ? local.minio_endpoint : module.create-seaweedfs.0.out_s3_endpoint
-  s3_credentials_secret       = local.use_minio_storage ? module.create-minio.0.out_minio_release_name : module.create-seaweedfs.0.out_s3_credentials_secret
-  s3_username_key             = local.use_minio_storage ? "root-user" : module.create-seaweedfs.0.out_s3_credentials_keys.argo_workflows_username
-  s3_password_key             = local.use_minio_storage ? "root-password" : module.create-seaweedfs.0.out_s3_credentials_keys.argo_workflows_password
+  s3_endpoint                 = var.minio_deploy ? local.minio_endpoint : module.create-seaweedfs.0.out_s3_endpoint
+  s3_credentials_secret       = var.minio_deploy ? module.create-minio.0.out_minio_release_name : module.create-seaweedfs.0.out_s3_credentials_secret
+  s3_username_key             = var.minio_deploy ? "root-user" : module.create-seaweedfs.0.out_s3_credentials_keys.argo_workflows_username
+  s3_password_key             = var.minio_deploy ? "root-password" : module.create-seaweedfs.0.out_s3_credentials_keys.argo_workflows_password
   archive_ttl                 = var.argo_archive_ttl
-  minio_release_name          = local.use_minio_storage ? module.create-minio.0.out_minio_release_name : ""
+  minio_release_name          = var.minio_deploy ? module.create-minio.0.out_minio_release_name : ""
   argo_bucket_name            = var.argo_bucket_name
   argo_database               = var.argo_database
   argo_postgresql_secret_name = var.argo_postgresql_secret_name
