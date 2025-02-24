d80cdfa9021ae1736bb5f946dc431a7383efc605
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-24 14:09:51 +0100

    add volume and master pvc seaweedfs

--- persistence-claim-seaweedfs/main.tf
+++ persistence-claim-seaweedfs/main.tf
@@ -1,10 +1,11 @@
 locals {
-  disk_name  = "disk-seaweedfs-tenant-${var.kubernetes_tenant_namespace}"
+  disk_master_name  = "disk-seaweedfs-tenant-${var.kubernetes_tenant_namespace}-master"
+  disk_volume_name  = "disk-seaweedfs-tenant-${var.kubernetes_tenant_namespace}-volume"
 }
 
 resource "kubernetes_persistent_volume_claim" "seaweedfs_master" {
   metadata {
-    name      = "pvc-${local.disk_name}"
+    name      = "pvc-${local.disk_master_name}"
     namespace = var.kubernetes_tenant_namespace
   }
   spec {
@@ -15,6 +16,23 @@ resource "kubernetes_persistent_volume_claim" "seaweedfs_master" {
         storage = "${var.pvc_seaweedfs_storage_gbi}Gi"
       }
     }
-    volume_name = "pv-${local.disk_name}"
+    volume_name = "pv-${local.disk_master_name}"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "seaweedfs_volume" {
+  metadata {
+    name      = "pvc-${local.disk_volume_name}"
+    namespace = var.kubernetes_tenant_namespace
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    storage_class_name = var.pvc_seaweedfs_storage_class_name
+    resources {
+      requests = {
+        storage = "${var.pvc_seaweedfs_storage_gbi}Gi"
+      }
+    }
+    volume_name = "pv-${local.disk_volume_name}"
   }
 }
