09ea327758bb0886eae188eb774cad38437e0367
nibaldo <nibaldo.donoso@cosmotech.com>
2025-03-11 15:29:55 +0100

    refactoies variables fmt [skip ci]

--- variables.argo.tf
+++ variables.argo.tf
@@ -1,71 +1,54 @@
 variable "argo_helm_repo_url" {
   type = string
 }
-
 variable "argo_helm_chart" {
   type = string
 }
-
 variable "argo_helm_chart_version" {
   type = string
 }
-
 variable "argo_service_account" {
   type = string
 }
-
 variable "argo_s3_endpoint" {
   type = string
 }
-
 variable "argo_s3_bucket_name" {
   type = string
 }
-
 variable "argo_s3_credentials_secret" {
   type = string
 }
-
 variable "argo_s3_username_key" {
   type = string
 }
-
 variable "argo_s3_password_key" {
   type = string
 }
-
 variable "argo_postgres_release_name" {
   type = string
 }
-
 variable "argo_database" {
   type = string
 }
-
 variable "argo_postgresql_secret_name" {
   type = string
 }
-
 variable "argo_requeue_time" {
   type = string
 }
-
 variable "argo_archive_ttl" {
   type = string
 }
-
 variable "argo_bucket_name" {
   type = string
 }
-
 variable "argo_deploy" {
   type = bool
 }
-
 variable "argo_install_crds" {
   type = bool
 }
-
 variable "argo_cloud_provider" {
   type = string
 }
--- variables.tenant.tf
+++ variables.tenant.tf
@@ -1,11 +1,9 @@
 variable "tenant_sp_client_id" {
   type = string
 }
-
 variable "tenant_sp_client_secret" {
   type = string
 }
-
 variable "tenant_resource_group" {
   type = string
 }
