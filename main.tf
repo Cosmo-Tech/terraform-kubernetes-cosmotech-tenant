387a419ac2efa7e340042d30cf4a117a299f1761
Sylvain Joubert <sylvain.joubert@cosmotech.com>
2025-04-07 14:50:04 +0200

    Fix summary variable names

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ data "terraform_remote_state" "tenant_infra" {
 data "template_file" "summary" {
   template = file("${path.module}/summary.tpl.md")
   vars = {
-    subscription_id      = data.terraform_remote_state.tenant_infra.outputs.out_azure_subscription_id
-    azure_resource_group = data.terraform_remote_state.tenant_infra.outputs.out_azure_tenant_resource_group
+    subscription_id      = data.terraform_remote_state.tenant_infra.outputs.out_subscription_id
+    azure_resource_group = data.terraform_remote_state.tenant_infra.outputs.out_tenant_resource_group
     azure_location       = data.terraform_remote_state.tenant_infra.outputs.out_azure_resource_location
 
     acr_login_server = data.terraform_remote_state.tenant_infra.outputs.out_acr_login_server
     rabbitmq_deploy  = var.rabbitmq_deploy
     adx_cluster_name = data.terraform_remote_state.tenant_infra.outputs.out_adx_cluster_name
-    adx_cluster_uri  = data.terraform_remote_state.tenant_infra.outputs.out_api_adx_uri
+    adx_cluster_uri  = data.terraform_remote_state.tenant_infra.outputs.out_adx_cluster_uri
     storage_name     = data.terraform_remote_state.tenant_infra.outputs.out_azure_storage_account_name
 
     argo_workflows_deploy   = var.argo_deploy
@@ -41,17 +41,13 @@ data "template_file" "summary" {
     redis_version           = var.redis_chart_version
     seaweedfs_chart_version = var.seaweedfs_helm_chart_version
 
-    babylon_sp_name      = data.terraform_remote_state.tenant_infra.outputs.out_babylon_sp_name
-    babylon_sp_client_id = data.terraform_remote_state.tenant_infra.outputs.out_babylon_sp_client_id
-    # platform_sp_name      = "cosmotech api client"
+    babylon_sp_name       = data.terraform_remote_state.tenant_infra.outputs.out_babylon_sp_name
+    babylon_sp_client_id  = data.terraform_remote_state.tenant_infra.outputs.out_babylon_sp_client_id
     platform_sp_name      = data.terraform_remote_state.tenant_infra.outputs.out_platform_sp_name
     platform_sp_client_id = data.terraform_remote_state.tenant_infra.outputs.out_tenant_sp_client_id
-    # platform_sp_client_id = module.create-keycloak.0.out_keycloak_api_client_id
     platform_sp_object_id = data.terraform_remote_state.tenant_infra.outputs.out_tenant_sp_object_id
-    # swagger_sp_name       = "cosmotech web client"
-    swagger_sp_name      = data.terraform_remote_state.tenant_infra.outputs.out_swagger_sp_name
-    swagger_sp_client_id = data.terraform_remote_state.tenant_infra.outputs.out_swagger_sp_client_id
-    # swagger_sp_client_id  = module.create-keycloak.0.out_keycloak_client_id
+    swagger_sp_name       = data.terraform_remote_state.tenant_infra.outputs.out_swagger_sp_name
+    swagger_sp_client_id  = data.terraform_remote_state.tenant_infra.outputs.out_swagger_sp_client_id
   }
 
   depends_on = [module.create-keycloak]
