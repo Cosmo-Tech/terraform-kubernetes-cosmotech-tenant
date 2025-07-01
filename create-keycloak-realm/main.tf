86c038a009aec9cfd2e33c3da64366a9b7aab5d7
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-07-01 17:41:34 +0200

    feat: Dedicated keycloak client for automatic testing

--- create-keycloak-realm/main.tf
+++ create-keycloak-realm/main.tf
@@ -165,6 +165,44 @@ resource "keycloak_user" "user_with_initial_password" {
     ]
   }
 }
+# automation client
+resource "keycloak_openid_client" "automation-client" {
+  realm_id                 = keycloak_realm.realm.id
+  client_id                = "automation-client"
+  name                     = "automation-client"
+  enabled                  = true
+  standard_flow_enabled    = false
+  access_type              = "CONFIDENTIAL"
+  service_accounts_enabled = true
+  login_theme              = "keycloak"
+  root_url                 = "https://${var.api_dns_name}"
+  full_scope_allowed       = true
+}
+
+resource "keycloak_generic_protocol_mapper" "automation_realm_roles_mapper" {
+  realm_id        = keycloak_realm.realm.id
+  client_id       = keycloak_openid_client.cosmotech-api-client.id
+  name            = "realm roles"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usermodel-realm-role-mapper"
+  config = {
+    "id.token.claim" : "true",
+    "access.token.claim" : "true",
+    "claim.name" : var.keycloak_realm_jwt_claim_api_client,
+    "jsonType.label" : "String",
+    "multivalued" : "true",
+    "userinfo.token.claim" : "true",
+    "introspection.token.claim" : "true"
+  }
+}
+
+resource "keycloak_openid_client_service_account_realm_role" "automation_client_service_account_role" {
+  count                   = var.keycloak_add_identity_provider_azure ? 1 : 0
+  realm_id                = keycloak_realm.realm.id
+  service_account_user_id = keycloak_openid_client.automation-client.service_account_user_id
+  role                    = keycloak_role.platform_admin_role.name
+  depends_on              = [keycloak_openid_client.automation-client]
+}
 
 # cosmotech api client
 resource "keycloak_openid_client" "cosmotech-api-client" {
